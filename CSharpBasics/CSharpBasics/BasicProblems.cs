f007063f762e09b0871ab03f013dfde2302c263f
Mónica Rivera <mriverap@outlook.com>
2020-04-23 13:02:06 -0700

    CSharpBasics initial solution

---
+++ CSharpBasics/CSharpBasics/BasicProblems.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpBasics
+{
+    public static class BasicProblems
+    {
+        public static void MyMethod1()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("method 1");
+        }
+        public static void MyMethod2()
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine("method 2");
+        }
+    }
+}
---
+++ CSharpBasics/CSharpBasics/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using static System.Int32;
+
+namespace CSharpBasics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Enter case (-1 to exit): ");
+                var userInput = Console.ReadLine();
+                TryParse(userInput, out var selection);
+
+                if (selection > 0)
+                {
+                    switch (selection)
+                    {
+                        case 1:
+                            BasicProblems.MyMethod1();
+                            break;
+                        case 2:
+                            BasicProblems.MyMethod2();
+                            break;
+                        default:
+                            Console.WriteLine("Nothing to do.");
+                            break;
+                    }
+                }
+                else if (selection == -1)
+                {
+                    Console.WriteLine("Exiting. Bye!");
+                    Console.ReadKey();
+                    return;
+                }
+            }
+        }
+    }
+}
